4b70ed297ec12ab246e2044902b561ada9876620
alectrico <ventas@alectrico.cl>
2024-07-19 07:37:36 -0400

    Agregando body a la accion de webhook

--- app/actions/messenger/webhook.rb
+++ app/actions/messenger/webhook.rb
@@ -3,7 +3,9 @@ module Bookis
     module Messenger
       class Webhook < Bookis::Action
         def handle(request, response)
+          Hanami.logger.info "en handle de Bookis_Action"
           response.status = 200
+          response.body = {:info => "En handle de Messenger::Webhook" }.to_json
         end
       end
     end
